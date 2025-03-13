08a12e615119661302d545bb14bd9e5eb6b46003
Kdiviya <eraiswaryabe@gmail.com>
2025-03-13 11:30:54 -0500

    Add 2dArrays.js

---
+++ 2dArrays.js
@@ -0,0 +1,25 @@
+//Challenge 1: Student Scores
+let allScores = [
+    [75,85,95],
+    [70,80,90],
+    [88,98,100]
+];
+console.log("Students Scores: ");
+console.log(allScores);
+
+//Challenge 2: Build a Seating Grid
+let theaterSeating = [
+    ["A1", "A2", "A3","A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+    ];
+console.log("Theatre Seating:");
+console.log(theaterSeating);
+
+//Challenge 3: Form a Multi-Dimensional Contact List
+let contactsList = [
+    ["Bob","bob@gmail.com", "530-423-7890"],
+    ["Charlie", "charlie@gmail.com", "123-456-7890"]
+];
+console.log("Contacts Info");
+console.log(contactsList);
